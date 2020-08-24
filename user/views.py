1b8ab842e0a12da559cbad0cfbd61bb02b16d280
HuZongHan <1602460860@qq.com>
2020-08-24 21:54:36 +0800

    用户相关功能

---
+++ user/views.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+# from user.models import User
+from flask import render_template
+
+user_bp = Blueprint('user', __name__, url_prefix='/user')
+user_bp.template_folder = './templates'
+
+
+@user_bp.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@user_bp.route('/login')
+def login():
+    # User.query.get(123)
+    return render_template('login.html')
+
+
+@user_bp.route('/info')
+def info():
+    return render_template('info.html')
